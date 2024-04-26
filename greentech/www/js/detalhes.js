52f9514ed7a6451045f4c4c9735d5449a23272bd
DarkSiderael <andre_muriel@outlook.com>
2024-04-26 12:47:35 -0300

    2

--- greentech/www/js/detalhes.js
+++ greentech/www/js/detalhes.js
@@ -15,6 +15,8 @@ if(item){
     $("#like-detalhe").html(item.likes);
     $("#reviews-detalhe").html(item.reviews + ' reviews');
     $("#descricao-detalhe").html(item.descricao);
+    $("#preco-detalhe").html('R$ '+ item.preco.toLocaleString('pt-BR', {Style: 'currency', currency:'BRL'}));
+    $("#precopromo-detalhe").html('R$ '+ item.preco_promocional.toLocaleString('pt-BR', {Style: 'currency', currency:'BRL'}));
 
     var tabelaDetalhes = $("#tabdetalhes");
 
@@ -33,5 +35,26 @@ if(item){
 
 } else {
     console.log('Produto não encontrado');
+}
+
+
+var carrinho= JSON.parse(localStorage.getItem('carrinho')) || [];
+
+fuction adicionarAoCarrinho(item, quantidade){
+    var itemNoCarrinho = carrinho.find(c=> c.item.id === item.id);
+
+    if(itemCarrinho){
+        itemNoCarrinho.quantidade += quantidade;
+        itemNoCarrinho.total_item = itemNoCarrinho.quantidade * item.preco_promocional;
+    }else{
+        carrinho.push({
+            item: item,
+            quantidade: quantidade,
+            total_item: quantidade * item.preco_promocional
+        })
+    }
+
+    localStorage.setItem('carrinho', JSON.stringify(carrinho));
+
 }
 
